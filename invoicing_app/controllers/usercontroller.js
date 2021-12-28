80ff51ce2f2aa1ea260aede433ea6b378b8bc6c4
Ritik Gupta <ritikgupta9414@gmail.com>
2021-12-28 21:59:15 +0530

    Controller for user added

--- invoicing_app/controllers/usercontroller.js
+++ invoicing_app/controllers/usercontroller.js
@@ -1,7 +1,36 @@
-exports.getOneUser=()=>{
+const userModel = require("./../models/user");
 
+exports.getOneUser = async (req, res) => {
+  try {
+    const user = await userModel.findById(req.params.userId);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).send("Error in getting user" + err);
+  }
 };
 
-exports.getAllUser=()=>{};
+exports.getAllUser = async (req, res) => {
+  try {
+    const allUser = await userModel.find({});
+    res.status(200).json(allUser);
+  } catch (err) {
+    res.status(500).send("Error in getting user" + err);
+  }
+};
 
-exports.updateUserInfo=()=>{};
+exports.updateUserInfo = async (req, res) => {
+  try {
+    const updatedEmail = req.body.email;
+    const updatedPassword = req.body.password;
+
+    const updateduser = await userModel.findById(req.params.userId);
+    updateduser.email = updatedEmail;
+    updateduser.password = updatedPassword;
+    updateduser.updateAt = new Date();
+    updateduser.save();
+
+    res.status(200).json(updateduser);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
--- invoicing_app/routes/userRouter.js
+++ invoicing_app/routes/userRouter.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const usercontroller = require('../controllers/usercontroller');
 
-router.get('/user/getUser/:userId',usercontroller.getOneUser);
-router.get('/user/allUsers',usercontroller.getAllUser);
-router.patch('/user/editPorfile/:userId',usercontroller.updateUserInfo);
+router.get('/getUser/:userId',usercontroller.getOneUser);
+router.get('/allUsers',usercontroller.getAllUser);
+router.patch('/editPorfile/:userId',usercontroller.updateUserInfo);
 
 module.exports=router;
 
