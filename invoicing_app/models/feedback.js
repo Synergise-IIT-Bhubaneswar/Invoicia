ed8ad83c5962fcd5d02bef62f83de33ca989b616
Ritik Gupta <ritikgupta9414@gmail.com>
2021-12-30 13:54:51 +0530

    feedback schema created

---
+++ invoicing_app/models/feedback.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const feedbackSchema = new Schema({
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  feedback: {
+    type: String,
+    required: [true, "Please write feedback message"],
+    minLength: [20, "Should contains atleast 20 words"],
+  },
+  rating: {
+    type: Number,
+    required: [true, "Rating must be specified wih feedback"],
+    max: 5,
+  },
+});
+
+module.exports = mongoose.model("feedback", feedbackSchema);
